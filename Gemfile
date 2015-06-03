9e56909bbee65f3af38045ac8c9b3520d9d3ec75
SirJustinWhite <Jjustinwhite@mac.com>
2015-06-02 23:00:33 -0500

    basic user model and secure passwords

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source 'https://rubygems.org'
 
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
+gem 'bcrypt'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  get 'users/new'
   get 'welcome/index'
   get 'welcome/example'
 
+  get 'signup' => 'users#new'
+  
   match '/example',    to: 'welcome#example',    via: 'get'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
