1808a35ca4c0206285a3d832dcd094b613b24c7a
SirJustinWhite <Jjustinwhite@mac.com>
2015-06-17 18:38:58 -0500

    photogrid update and fonts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603052350) do
+ActiveRecord::Schema.define(version: 20150613005825) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150603052350) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.string   "avatar_url"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
