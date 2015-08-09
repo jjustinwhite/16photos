13589c164b1e45e256a63378619cce150c058bd1
SirJustinWhite <Jjustinwhite@mac.com>
2015-08-08 20:29:49 -0500

    user show view UI update / new url for post show view

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,7 @@ class Post < ActiveRecord::Base
 	validates :image, presence: true
 
 
-	has_attached_file :image, styles: { :medium => "640x480", :thumb => "300x300#"}
+	has_attached_file :image, styles: { :medium => "640x480", :thumb => "300x300#"},
+					  :convert_options => { :medium => "-quality 80 -interlace Plane" }
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end  
